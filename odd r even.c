493a1421734a49519f6224d022b34f9632c96275
KUPPURAJM <34646140+KUPPURAJM@users.noreply.github.com>
2018-03-05 11:59:31 +0530

    Create odd r even.c

---
+++ odd r even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a=4;
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+    	printf("even");
+    	
+    }else
+    {
+    	printf("odd");
+    }
+	return 0;
+}
